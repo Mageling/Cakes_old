937f8e106300b3623ca9689520e3c762003d9486
Mageling <nksadow@gmail.com>
2013-04-21 12:55:28 -0600

    Shifted Packages and fixed imports

---
+++ cakes_common/mageling/cake/core/proxy/CommonProxy.java
@@ -0,0 +1,11 @@
+package mageling.cake.core.proxy;
+
+public class CommonProxy {
+    public static String ITEMS_PNG = "";
+    public static String BLOCKS_PNG = "";
+    
+    public void registerRenderers() {
+        
+    }
+
+}
---
+++ cakes_common/mageling/cake/item/ItemCakeTnt.java
@@ -0,0 +1,13 @@
+package mageling.cake.item;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemReed;
+
+public class ItemCakeTnt extends ItemReed {
+
+    public ItemCakeTnt(int par1, Block par2Block) {
+        super(par1, par2Block);
+        this.maxStackSize = 1;
+    }
+
+}
